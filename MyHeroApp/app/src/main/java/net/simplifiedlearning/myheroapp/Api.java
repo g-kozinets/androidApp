7fbe4ac81581943ef45945da8be6993becab4152
glebokator <glebokator@gmail.com>
2018-05-15 09:06:01 +0300

    Add files via upload

---
+++ MyHeroApp/app/src/main/java/net/simplifiedlearning/myheroapp/Api.java
@@ -0,0 +1,12 @@
+package net.simplifiedlearning.myheroapp;
+
+public class Api {
+
+    private static final String ROOT_URL = "http://192.168.0.4/HeroApi/v1/Api.php?apicall=";
+
+    public static final String URL_CREATE_HERO = ROOT_URL + "createhero";
+    public static final String URL_READ_HEROES = ROOT_URL + "getheroes";
+    public static final String URL_UPDATE_HERO = ROOT_URL + "updatehero";
+    public static final String URL_DELETE_HERO = ROOT_URL + "deletehero&id=";
+
+}
